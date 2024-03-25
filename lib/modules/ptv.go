9d05544c39535c61c95aa676eb88b79acf9eac64
RolandWarburton <warburtonroland@gmail.com>
2024-03-25 17:56:47 +1100

    re-write PTV module again

--- lib/modules/ptv.go
+++ lib/modules/ptv.go
@@ -1,8 +1,8 @@
 package modules
 
 import (
+	"errors"
 	"fmt"
-	"math"
 	"time"
 
 	ptv "github.com/rolandwarburton/ptv-status-line/pkg"
@@ -14,82 +14,109 @@ type PublicTransport struct {
 	NextDeparture time.Time
 	LastPoll      time.Time
 	nextPoll      time.Time
-	Status        string
 }
 
-func (m *PublicTransport) poll() {
-	// avoid sending more than one request at a time
-	if m.Status == "polling" {
-		return
-	}
-	m.Status = "polling"
-	departures, err := ptv.DeparturesAction("Lilydale", "Southern Cross", "Lilydale", 3, "Australia/Sydney")
+func parseStringToTime(timeString string) (time.Time, error) {
+	var departureTime time.Time
+	layout := "02-01-2006 03:04 PM"
+	departureTime, err := time.Parse(layout, timeString)
 	if err != nil {
-		m.Status = "error"
-		return
+		fmt.Println(err)
+		return time.Time{}, errors.New("error parsing time")
+	}
+	return departureTime, nil
+}
+
+func parseTimeToLocaleString(o time.Time) string {
+	// convert the departure time into a 12h short format (HH:MM)
+	location, err := time.LoadLocation("Australia/Sydney")
+	if err != nil {
+		return ""
+	}
+
+	secUntilDeparture := time.Until(o).Seconds()
+	var departureTimeString string
+	if secUntilDeparture < 0 {
+		departureTimeString = "waiting for next train"
+		return fmt.Sprintf("%s %.0f", departureTimeString, secUntilDeparture)
+	} else {
+		departureTimeString = o.In(location).Format("03:04 PM")
+		return fmt.Sprintf("Train in %.0fmin (%s)", secUntilDeparture, departureTimeString)
+	}
+}
+
+func (m *PublicTransport) calcPollSleepTime() time.Duration {
+
+	// set the sleep time based on the last train departure
+	var sleepTime time.Duration
+
+	nextDepartureTime, err := parseStringToTime(m.Departures[0].ScheduledDepartureUTC)
+
+	if err != nil {
+		return 5 * time.Minute
+	}
+
+	// default time to wait until next poll
+	nextDepartureSeconds := time.Until(nextDepartureTime).Seconds()
+
+	if nextDepartureSeconds <= 0 {
+		sleepTime = 10 * time.Second
+	} else {
+		sleepTime = time.Duration(nextDepartureSeconds+10) * time.Second
+	}
+	return sleepTime
+}
+
+func (m *PublicTransport) poll() (sleep time.Duration) {
+	fmt.Println("poll")
+	departures, err := ptv.DeparturesAction("Belgrave", "Southern Cross", "Belgrave", 1, "Australia/Sydney")
+	if err != nil || len(departures) == 0 {
+		timeout := 5 * time.Minute
+		m.nextPoll = time.Now().Add(timeout)
+		return time.Duration(timeout)
 	}
 	m.Departures = departures
 	m.LastPoll = time.Now()
-	m.Status = "done"
+	scheduledDepartureTime, _ := parseStringToTime(departures[0].ScheduledDepartureUTC)
+	m.NextDeparture = scheduledDepartureTime
+
+	sleepTime := m.calcPollSleepTime()
+	m.nextPoll = time.Now().Add(sleepTime)
+	return sleepTime
 }
 
-func (m *PublicTransport) Init() {
-	m.Enabled = true
-	m.Status = "loading..."
+func (m *PublicTransport) startPoll() {
 	go func() {
 		for {
-			m.poll()
-			// set the sleep time based on the last train departure
-			sleepTime := 5 * time.Minute
-			var seccondsUntilDeparture float64
-
-			// if we are loading then wait 5s to throttle retries
-			if m.Status == "loading..." {
-				seccondsUntilDeparture = 5
-			} else {
-				seccondsUntilDeparture = time.Until(m.NextDeparture).Seconds()
-			}
-
-			// if the train left
-			if seccondsUntilDeparture < 0 {
-				sleepTime = time.Duration(0)
-			}
-
-			// if the train is going to leave in a long time slow down
-			if seccondsUntilDeparture > 600 {
-				sleepTime = time.Duration(int(math.Ceil(seccondsUntilDeparture/2))) * time.Minute
-			}
-			m.nextPoll = time.Now().Add(sleepTime)
+			sleepTime := m.poll()
 			time.Sleep(sleepTime)
 		}
 	}()
 }
 
+func (m *PublicTransport) Init() {
+	m.Enabled = true
+	m.nextPoll = time.Now().Add(1 * time.Second)
+	m.startPoll()
+}
+
 func (m *PublicTransport) Run() string {
-	if len(m.Departures) == 0 {
-		return m.Status
+	// test if there is anything to print
+	if m.NextDeparture.Year() == 1 {
+		nextDepartureSeconds := time.Until(m.nextPoll).Seconds()
+		return fmt.Sprintf("polling in %.0f", nextDepartureSeconds)
 	}
-	departure := m.Departures[0]
 
-	// convert the departure time into a 12h short format (HH:MM)
 	var location *time.Location
-	layout := "02-01-2006 03:04 PM"
-	departureTime, err := time.Parse(layout, departure.ScheduledDepartureUTC)
-	m.NextDeparture = departureTime
-	if err != nil {
-		fmt.Println(err)
-		return "error"
-	}
-	location, err = time.LoadLocation("Australia/Sydney")
+	location, err := time.LoadLocation("Australia/Sydney")
 	if err != nil {
 		return "error"
 	}
 
-	timeUntilDeparture := time.Until(departureTime).Minutes()
-	departureTimeString := departureTime.In(location).Format("03:04 PM")
+	timeUntilDeparture := time.Until(m.NextDeparture).Minutes()
+	departureTimeString := m.NextDeparture.In(location).Format("03:04 PM")
 	if timeUntilDeparture < 0 {
 		departureTimeString = "waiting for next train"
-		m.poll()
 		nextDepartureSeconds := time.Until(m.NextDeparture).Seconds()
 		return fmt.Sprintf("%s %.0f", departureTimeString, nextDepartureSeconds)
 	} else {
