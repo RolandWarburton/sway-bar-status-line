b188e9a4c9941412d248c0ea74ef1f4cc97c0c0b
RolandWarburton <warburtonroland@gmail.com>
2024-05-06 20:26:16 +1000

    Another rewrite

--- lib/modules/ptv.go
+++ lib/modules/ptv.go
@@ -2,7 +2,6 @@ package modules
 
 import (
 	"fmt"
-	"math"
 	"time"
 
 	ptv "github.com/rolandwarburton/ptv-status-line/pkg"
@@ -14,85 +13,94 @@ type PublicTransport struct {
 	NextDeparture time.Time
 	LastPoll      time.Time
 	nextPoll      time.Time
-	Status        string
+	isPolling     bool
 }
 
 func (m *PublicTransport) poll() {
 	// avoid sending more than one request at a time
-	if m.Status == "polling" {
+	defer func() {
+		m.isPolling = false
+	}()
+	if m.isPolling {
 		return
 	}
-	m.Status = "polling"
+	m.isPolling = true
 	departures, err := ptv.DeparturesAction("Lilydale", "Southern Cross", "Lilydale", 3, "Australia/Sydney")
 	if err != nil {
-		m.Status = "error"
 		return
 	}
 	m.Departures = departures
 	m.LastPoll = time.Now()
-	m.Status = "done"
+	m.LastPoll = time.Now()
+	m.isPolling = false
 }
 
 func (m *PublicTransport) Init() {
 	m.Enabled = true
-	m.Status = "loading..."
+	m.nextPoll = time.Now()
 	go func() {
 		for {
 			m.poll()
-			// set the sleep time based on the last train departure
+			// sleep 5min
 			sleepTime := 5 * time.Minute
-			var seccondsUntilDeparture float64
-
-			// if we are loading then wait 5s to throttle retries
-			if m.Status == "loading..." {
-				seccondsUntilDeparture = 5
-			} else {
-				seccondsUntilDeparture = time.Until(m.NextDeparture).Seconds()
-			}
-
-			// if the train left
-			if seccondsUntilDeparture < 0 {
-				sleepTime = time.Duration(0)
-			}
-
-			// if the train is going to leave in a long time slow down
-			if seccondsUntilDeparture > 600 {
-				sleepTime = time.Duration(int(math.Ceil(seccondsUntilDeparture/2))) * time.Minute
-			}
 			m.nextPoll = time.Now().Add(sleepTime)
 			time.Sleep(sleepTime)
 		}
 	}()
 }
 
-func (m *PublicTransport) Run() string {
-	if len(m.Departures) == 0 {
-		return m.Status
-	}
-	departure := m.Departures[0]
+type DepartureTimingInformation struct {
+	DepartureTime         time.Time
+	MinutesUntilDeparture float64
+	Location              *time.Location
+}
 
-	// convert the departure time into a 12h short format (HH:MM)
+func GetDepartureTimingInformation(departure ptv.Departure) (*DepartureTimingInformation, error) {
 	var location *time.Location
 	layout := "02-01-2006 03:04 PM"
 	departureTime, err := time.Parse(layout, departure.ScheduledDepartureUTC)
-	m.NextDeparture = departureTime
 	if err != nil {
-		fmt.Println(err)
-		return "error"
+		return nil, err
 	}
 	location, err = time.LoadLocation("Australia/Sydney")
 	if err != nil {
-		return "error"
+		return nil, err
+	}
+	minutesUntilDeparture := time.Until(departureTime).Minutes()
+	return &DepartureTimingInformation{
+		DepartureTime:         departureTime,
+		MinutesUntilDeparture: minutesUntilDeparture,
+		Location:              location,
+	}, nil
+}
+
+func (m *PublicTransport) Run() string {
+	if len(m.Departures) == 0 {
+		secondsUntilNextPoll := time.Until(m.nextPoll).Seconds()
+		isPollingStr := map[bool]string{true: " (polling)", false: ""}[m.isPolling]
+		return fmt.Sprintf("No data available%s (%.0f)", isPollingStr, secondsUntilNextPoll)
 	}
 
-	timeUntilDeparture := time.Until(departureTime).Minutes()
-	departureTimeString := departureTime.In(location).Format("03:04 PM")
-	if timeUntilDeparture < 0 {
-		departureTimeString = "waiting for next train"
-		m.poll()
-		nextDepartureSeconds := time.Until(m.NextDeparture).Seconds()
-		return fmt.Sprintf("%s %.0f", departureTimeString, nextDepartureSeconds)
-	} else {
-		return fmt.Sprintf("Train in %.0fmin (%s)", timeUntilDeparture, departureTimeString)
+	var result string
+	for i, departure := range m.Departures {
+		info, err := GetDepartureTimingInformation(departure)
+		if err != nil {
+			result = "error"
+			break
+		}
+
+		if info.MinutesUntilDeparture < 0 && i == len(m.Departures)-1 {
+			result = "No data available"
+			break
+		}
+		departureTimeString := info.DepartureTime.In(info.Location).Format("03:04 PM")
+		isPollingStr := map[bool]string{true: " (polling)", false: ""}[m.isPolling]
+		result = fmt.Sprintf(
+			"Train in %.0fmin (%s)%s",
+			info.MinutesUntilDeparture,
+			departureTimeString,
+			isPollingStr,
+		)
 	}
+	return result
 }
