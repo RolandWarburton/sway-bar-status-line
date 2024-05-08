f781a717076cb0117cf590f000ed08116d265b1b
RolandWarburton <warburtonroland@gmail.com>
2024-05-08 18:31:27 +1000

    Update config.go to allow reading from ENV

--- config.go
+++ config.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"path"
 
 	"gopkg.in/yaml.v3"
 )
@@ -28,10 +30,37 @@ func defaultConfig() *Config {
 	return defaultConfig
 }
 
+func getConfigPath() (*string, error) {
+	var home, err = os.UserHomeDir()
+	if err != nil {
+		// fall back on the default config
+		return nil, errors.New("failed to get users home")
+	}
+
+	configLocation, cfgPathSet := os.LookupEnv("SWAYBAR_CONFIG_LOCATION")
+
+	if !cfgPathSet {
+		// default to ~/.config/swaybar/config.yml
+		p := path.Join(home, ".config/swaybar/config.yml")
+		return &p, nil
+	}
+
+	if _, err := os.Stat(configLocation); err != nil {
+		// fall back on the default config
+		return nil, errors.New("SWAYBAR_CONFIG_LOCATION does not exist")
+	}
+
+	return &configLocation, nil
+}
+
 func getConfig() *Config {
 	defaultConfig := defaultConfig()
+	configPath, err := getConfigPath()
+	if err != nil {
+		return defaultConfig
+	}
 
-	f, err := os.ReadFile("./config.yml")
+	f, err := os.ReadFile(*configPath)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to read config")
 		return defaultConfig
